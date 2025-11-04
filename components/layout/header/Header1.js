cf003a52511ccc605f0ef2ba1faeff3869b5ffd5
Muhammad Saad Asghar <saadasghar3606@gmail.com>
2025-11-04 12:30:49 +0400

    fixing

--- components/layout/header/Header1.js
+++ components/layout/header/Header1.js
@@ -9,7 +9,7 @@ export default function Header1({ scroll, isMobileMenu, handleMobileMenu, isSide
             <header className={`main-header main-header-one container ${scroll ? "" : ""}`}>
                 <div className={`menu-area ${scroll ? "sticky-menu" : ""}`}>
                     {/* header-lower */}
-                    <div className="container">
+                     <div style={{paddingLeft: 0, paddingRight: 0}} className={`menu-area ${scroll ? "spacing_ending" : ""} container`}>
                         <div className="menu-area__inner">
                             <div className="mobile-nav-toggler" onClick={handleMobileMenu}>
                                 {/* <i className="fas fa-bars"></i> */}
@@ -23,7 +23,7 @@ export default function Header1({ scroll, isMobileMenu, handleMobileMenu, isSide
   <Image
     src="/assets/icon/emaar_logo.png"
     alt="emaar_logo"
-    height={60}
+    height={70}
     width={300}
     className="emaar_logo"
   />
