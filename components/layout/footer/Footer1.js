cfef6c481252dfe59f00cb6f494ee8bc9ea09f96
Muhammad Saad Asghar <saadasghar3606@gmail.com>
2025-11-05 13:14:36 +0400

    replace c

--- components/layout/footer/Footer1.js
+++ components/layout/footer/Footer1.js
@@ -40,7 +40,7 @@ export default function Footer1() {
 
                                      <div className="footer-widget__about-inner footer_spacing privacy_policy_footer centerize" style={{marginTop: 0}}>
                                         <div className="number-box centerize">
-                                            <span style={{color: "#ffffff"}}>@ SHIRO ESTATE {currentYear}. All Rights Reserved.</span>
+                                            <span style={{color: "#ffffff"}}>&copy; SHIRO ESTATE {currentYear}. All Rights Reserved.</span>
                                         </div>
                                     </div>
                                 </div>
